8ff30fd5321376023babbe34b962bf63ddc87f40
LuizaNs <luizansjs@gmail.com>
2024-10-14 08:39:46 -0300

    Upload do projeto final. Dockerfile adicionado.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using MAI.Persistencia;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2TDSPM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MAIDbContext>(options =>
+            {
+                options.UseOracle(builder.Configuration.GetConnectionString("OracleFIAP"));
+            });
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
